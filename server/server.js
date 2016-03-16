5a81d804feff6766d0516e6e10c1d26416034777
Aivars Sazanovics <aivars.sazanovics@accenture.com>
2016-03-16 13:52:05 +0200

    Disabling prod mode

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
 var http            = require('http'),
 	path 			= require('path'),
     express         = require('express'),
-    bodyParser      = require('body-parser');
+    bodyParser      = require('body-parser'),
+	app 			= express();
 
-var app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.set('views', path.join(__dirname, '../client'));
 app.set('view engine', 'ejs');
+app.use(express.static("../client"));
 app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/', require('./routes/index'));
 app.use('/api/users/', require('./routes/users'));
 app.use('/api/todo/', require('./routes/todo'));
 app.use('/api/protected/', require('./routes/protected-routes'));
 
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3002;
 http.createServer(app).listen(port, function (err) {
   console.log('listening in http://localhost:' + port);
 });
